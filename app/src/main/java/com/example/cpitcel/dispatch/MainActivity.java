3c019b4c4bfbc289062a8df75d173335229c54c7
Cameron <cpitcel@illinois.edu>
2018-10-09 16:17:50 -0500

    Progress

--- app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
+++ app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cpitcel.dispatch;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
@@ -20,6 +23,21 @@ public class MainActivity extends ListActivity {
     }
 
     class RowAdapter extends ArrayAdapter<String> {
-        
+
+        RowAdapter() {
+            super(MainActivity.this, R.layout.myrow, users);
+        }
+
+        public View getView(int position, View theView, ViewGroup parentView){
+            View myView = theView;
+
+            if (myView == null) { //only do if it does not exist, so no resource wasting
+                LayoutInflater inflaterator = getLayoutInflater();
+            }
+
+            RowView theRow = (RowView) myView.getTag();
+
+            return theView;
+        }
     }
 }
