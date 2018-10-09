015351d2bf4c6e27f1a3e0a3a67ed364aea974dd
Cameron <cpitcel@illinois.edu>
2018-10-09 14:41:38 -0500

    Additions

--- app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
+++ app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.cpitcel.dispatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    private static final String[] users = {"Doug","Bill", "Matt","Chris", "Shaq","Nathan","Tim",
+                                            "Alex","Brandon"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setListAdapter(new );
+    }
+
+    class RowAdapter extends ArrayAdapter<String> {
+        
     }
 }
