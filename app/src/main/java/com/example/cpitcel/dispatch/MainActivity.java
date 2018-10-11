5af2a5d8f22a3b27f9a047370aa29d357ed3c98c
Cameron <cpitcel@illinois.edu>
2018-10-11 11:20:17 -0500

    Should be set.
    If only it would work!

--- app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
+++ app/src/main/java/com/example/cpitcel/dispatch/MainActivity.java
@@ -2,28 +2,29 @@ package com.example.cpitcel.dispatch;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.util.Log;
 import android.widget.ListAdapter;
 
 public class MainActivity extends ListActivity {
 
     private static final String[] users = {"Doug","Bill", "Matt","Chris", "Shaq","Nathan","Tim",
                                             "Alex","Brandon"};
-
+    private static final String tag = "Dev:";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //setListAdapter(new );
+        setContentView(R.layout.activitymain);
+        Log.i(tag, "onCreate");
+        setListAdapter(new RowAdapter());
     }
 
-    class RowAdapter extends ArrayAdapter<String> {
-
+    public class RowAdapter extends ArrayAdapter<String> {
         RowAdapter() {
             super(MainActivity.this, R.layout.myrow, users);
         }
@@ -33,9 +34,18 @@ public class MainActivity extends ListActivity {
 
             if (myView == null) { //only do if it does not exist, so no resource wasting
                 LayoutInflater inflaterator = getLayoutInflater();
+                myView = inflaterator.inflate(R.layout.myrow, parentView, false);
             }
 
             RowView theRow = (RowView) myView.getTag();
+
+            if (theRow == null){
+                theRow = new RowView(myView);
+                myView.setTag(theRow);
+            }
+
+            //Assign values for theRow
+            theRow.userName.setText(users[position]);
 
             return theView;
         }
